2e950c41a6587204293c4f552f47fc87686ab1ba
Erwin Wu <32383242+erwinwu211@users.noreply.github.com>
2018-11-06 20:56:48 +0900

    Fixed Terrain Height

---
+++ SkiSimu-Unity/Assets/FixedTerrainHeight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedTerrainHeight : MonoBehaviour {
+    private float initialHeight;
+    public float roomHeight;
+    private float deltaHeight;
+	// Use this for initialization
+	void Start () {
+        initialHeight= Terrain.activeTerrain.SampleHeight(transform.position);
+        deltaHeight = roomHeight/2f - 0.35f;
+    }
+
+    // Update is called once per frame
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;  //Set height to terrain height + half of cube height
+        pos.y = Terrain.activeTerrain.SampleHeight(transform.position) - initialHeight + deltaHeight;
+        transform.position = pos;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0); // Set Z axis rotation to Zero
+    }
+    
+}
+
